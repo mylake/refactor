16a6ff906901df1f633fa4878905c514bd469132
Lake Chang <lake.chang@vpon.com>
2015-12-10 15:20:59 +0530

    6_9

---
+++ ch6/6_9.rb
@@ -0,0 +1,36 @@
+class Account
+  def gamma(input_val, quantity, year_to_date)
+    Gamma.new(self, input_val, quantity, year_to_date)
+  end
+end
+
+class Gamma
+  attr_reader :account,
+    :input_val,
+    :quantity,
+    :year_to_date,
+    :important_value1,
+    :important_value2,
+    :important_value3
+
+  def initialize(account, input_val, quantity, year_to_date)
+    @account = account
+    @input_val = input_val
+    @quantity = quantity
+    @year_to_date = year_to_date
+  end
+
+  def compute
+    @important_value1 = (input_val * quantity) + @account.delta
+    @important_value2 = (input_val * year_to_date) + 100
+    important_thing
+    @important_value3 = @important_value2 * 7
+    @important_value3 - 2 * @important_value1
+  end
+
+  def important_thing
+    if (year_to_date - @important_value1) > 100
+      @important_value1 -= 20
+    end
+  end
+end
