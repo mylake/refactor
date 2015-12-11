3cac8b932e47ce92cf3b1ccc6b47d6db41d7e93f
Lake Chang <lake.chang@vpon.com>
2015-12-11 17:16:07 +0530

    6_14

---
+++ ch6/6_14.rb
@@ -0,0 +1,32 @@
+module AssertValidKeys
+  def assert_valid_keys(*valid_keys)
+    unknown_keys = keys - [valid_keys].flatten
+    if unknown_keys.any?
+      raise(ArgumentError, "Unknown keys(s): #{unknown_keys.join(', ')}")
+    end
+  end
+end
+
+Hash.send(:include, AssertValidKeys)
+
+
+class Books
+  def self.find(selector, hash={})
+    hash.assert_valid_keys :conditions, :joins
+
+    hash[:joins] ||= []
+    hash[:conditions] ||= ""
+
+    sql = ["SELECT * FROM books"]
+    hash[:joins].each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table} ON"
+      sql << "books.#{join_table.to_s.chap}_id"
+      sql << " = #{join_table}.id"
+    end
+
+    sql << "WHERE #{hash[:conditions]}" unless {hash[:conditions]}.empty?
+    sql << "LIMIT 1" if selector == :first
+
+    condition.find(sql.join(" "))
+  end
+end
---
+++ ch6/6_14_before.rb
@@ -0,0 +1,14 @@
+class Books
+  def self.find(selector, conditions="", *joins)
+    sql = ["SELECT * FROM books"]
+    joins.each do |join_table|
+      sql << "LEFT OUTER JOIN #{join_table} ON"
+      sql << "books.#{join_table.to_s.chap}_id"
+      sql << " = #{join_table}.id"
+    end
+    sql << "WHERE #{conditions}" unless conditions.empty?
+    sql << "LIMIT 1" if selector == :first
+
+    condition.find(sql.join(" "))
+  end
+end
